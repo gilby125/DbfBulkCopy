f9c8373883ef8e41e527ebaf0d0ae172cd035590
Gilby <gilby125@users.noreply.github.com>
2021-08-24 16:09:22 -0500

    Delete append_stl.ps1

--- append_stl.ps1
+++
@@ -1,3 +0,0 @@
-
-$yesterday = (get-date).AddDays(-1).ToString("MMMdd_yy") + ".CS"
-Start-Process  -FilePath "C:\DBFCopy\DbfBulkCopy.exe" -ArgumentList "--server 10.129.19.44 --database Agile  --userid agile --password Password2 --dbf \\stlcs01\BluJay\DAILY_ARCHIVE\$yesterday  --table CS_STL_ARCHIVE"  
